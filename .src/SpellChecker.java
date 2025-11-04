bd938c2eff25dcaf47689bafbad600ff4730894f
anyacui <anyacui@seas.upenn.edu>
2025-11-03 23:10:36 -0500

    Added helper methods

--- .src/SpellChecker.java
+++ .src/SpellChecker.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,33 +14,58 @@ public class SpellChecker {
 
     public void start() {
         // TODO: Complete the body of this method, as necessary
-        System.out.printf(Util.DICTIONARY_PROMPT);
-        System.out.print(">> ");
-        String fileName = inputReader.next();
-
-        // Continue asking user to input dictionary name until successfully opened
-        while(!inputDict(fileName)){
-            System.out.printf(Util.DICTIONARY_PROMPT);
-            System.out.print(">> ");
-            fileName = inputReader.next();
-        }
+        String dictName = inputDict();
         // Dictionary is successfully opened
-        System.out.printf(Util.DICTIONARY_SUCCESS_NOTIFICATION, fileName);
+        System.out.printf(Util.DICTIONARY_SUCCESS_NOTIFICATION, dictName);
+
+        String fileName = inputFile();
+        // Dictionary is successfully opened
+        System.out.printf(Util.FILE_SUCCESS_NOTIFICATION, fileName, fileName + "_chk");
 
         inputReader.close();  // DO NOT MODIFY - must be the last line of this method!
 
     }
 
-    // You can of course write other methods as well.
-    private boolean inputDict(String fileName){
+    // Helper function to check if valid file
+    private boolean validFile(String fileName) {
         try {
-            FileInputStream input = new FileInputStream(".src/"+fileName);
+            FileInputStream input = new FileInputStream(".src/" + fileName);
             return true;
 
         } catch (IOException e) {
             System.out.printf(Util.FILE_OPENING_ERROR);
             return false;
         }
+    }
+
+    // Helper method to ask user for dictionary to use
+    private String inputDict() {
+        System.out.printf(Util.DICTIONARY_PROMPT);
+        System.out.print(">> ");
+        String fileName = inputReader.next();
+
+        // Continue asking user to input dictionary name until successfully opened
+        while (!validFile(fileName)) {
+            System.out.printf(Util.DICTIONARY_PROMPT);
+            System.out.print(">> ");
+            fileName = inputReader.next();
+        }
+        return fileName;
+    }
+
+    // Helper method to ask user for file to spellcheck
+    private String inputFile() {
+        System.out.printf(Util.FILENAME_PROMPT);
+        System.out.print(">> ");
+        String inputName = inputReader.next();
+
+        // Continue asking user to input file name until successfully opened
+        while (!validFile(inputName)) {
+            System.out.printf(Util.FILENAME_PROMPT);
+            System.out.print(">> ");
+            inputName = inputReader.next();
+        }
+        return inputName;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
